a54744307f292247d50b4ef8c52d75f1fe0d1ce5
Minoru-Abe <tomominoru.abe@gmail.com>
2022-04-03 16:16:58 +0900

    commit Program1_11 and Program1_12

---
+++ java-sample/src/kitapractice/Program1_11.java
@@ -0,0 +1,13 @@
+package kitapractice;
+
+public class Program1_11 {
+	public static void main(String args[]) {
+		int x = 3;
+		int y = 2;
+		
+		int quotient  = x / y;
+		int remainder = x % y;
+		System.out.println(x + "/" + y +"= " + quotient + " remainder is " + remainder);
+	}
+
+}
---
+++ java-sample/src/kitapractice/Program1_12.java
@@ -0,0 +1,14 @@
+package kitapractice;
+
+public class Program1_12 {
+	public static void main(String args[]) {
+		int x = 1;
+		for (int i = 0; i < 10; i++) {
+			System.out.println(x + "+" + i + " = " + (x + i));
+		}
+		for (int j = 10; j > 0; j--) {
+			System.out.println(x + "+" + j + " = " + (x + j));
+		}
+	}
+
+}
