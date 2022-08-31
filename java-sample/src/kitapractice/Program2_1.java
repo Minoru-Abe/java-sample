7f58fc40bd0ad06b69f027605a3e9caa1743f33c
Minoru-Abe <tomominoru.abe@gmail.com>
2022-09-01 06:03:06 +0900

    Kita practice

---
+++ java-sample/src/kitapractice/Program2_1.java
@@ -0,0 +1,15 @@
+package kitapractice;
+import java.io.*;
+
+
+public class Program2_1 {
+	public static void main(String args[]) throws IOException{
+		BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+		String s = br.readLine();
+		
+		System.out.println(s);
+		
+		
+	}
+
+}
---
+++ java-sample/src/kitapractice/Program2_2.java
@@ -0,0 +1,15 @@
+package kitapractice;
+import java.io.*;
+
+public class Program2_2 {
+	public static void main(String args[]) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		int n = Integer.parseInt(s);
+		
+		System.out.println(n);
+				
+		
+	}
+
+}
